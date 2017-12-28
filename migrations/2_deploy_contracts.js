d7dc865b41276fe681fa6651e7f43bb06e2817e3
Nicolas Luck <nicolas@lucksus.eu>
2017-12-29 00:05:59 +0100

    Testnet deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,11 +6,11 @@ module.exports = function(deployer, network, accounts) {
   let sale, whitelist
   deployer.deploy(HoloToken).then(() => {
     wei_to_eth = 1000000000000000000
-    eth_to_eur = 567
-    rate = wei_to_eth * eth_to_eur * 10000
-    min_wei_20_eur = web3.toWei(1, 'ether') * 20 / eth_to_eur
-    blocks_in_3_days = 60*60*24*3 / 15
-    return deployer.deploy(HoloSale, web3.eth.blockNumber + 10, web3.eth.blockNumber + blocks_in_3_days, rate, min_wei_20_eur, 10, accounts[0])
+    eth_to_eur = 638
+    rate = wei_to_eth * eth_to_eur * 10000 * 100000
+    min_wei_20_eur = web3.toWei(1, 'ether') * 20 / eth_to_eur / 100000
+    blocks_in_7_days = 60*60*24*7 / 15
+    return deployer.deploy(HoloSale, web3.eth.blockNumber + 10, web3.eth.blockNumber + blocks_in_7_days, rate, min_wei_20_eur, 10, accounts[0])
   }).then(() => {
     return deployer.deploy(HoloWhitelist)
   }).then(() => {
